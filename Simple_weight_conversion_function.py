1c9bb397e47add7bb80fdbfba96c3e0e3555ac07
K <91447359+komanrath@users.noreply.github.com>
2023-09-26 16:42:44 -0400

    Add Simple_weight_conversion_function.py
    Working version

--- Simple_weight_conversion_function.py
+++ Simple_weight_conversion_function.py
@@ -2,7 +2,7 @@
 
 Weight = float(input("Weight: "))
 Units = input("(K)g or (L)bs? ")
-if ("l" or "L") in Units: 
+if Units.upper() == "L": 
     print("Weight in Kg: " + str(Weight / 2.2046))
 else:
     print("Weight in Lbs: " + str(Weight * 2.2046))
